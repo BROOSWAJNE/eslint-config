9b903d07c70fadf12496b378e53430151536900f
Willem Garnier <willem.garnier@checkit.net>
2022-01-27 17:14:34 +0000

    disable some default rules for mocha

--- overrides/mocha.js
+++ overrides/mocha.js
@@ -13,6 +13,16 @@ module.exports = {
 
 		"mocha/max-top-level-suites": "warn",
 		"mocha/no-exports": "warn",
+
+		// Disables
+
+		// each describe() and it() gets counted as an extra level of callback
+		// nesting, but we want to allow those
+		"max-nested-callbacks": "off",
+		// the exact number of something which we expect isn't magic
+		"no-magic-numbers": "off",
+		// a single describe() might be verrryyy long
+		"max-lines-per-function": "off",
 	},
 
 };
